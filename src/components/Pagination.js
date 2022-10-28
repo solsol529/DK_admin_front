00e3d1e31418e3908a355b3f705a3e0a539ffc08
EunjiShinRabbit <dmswll97@gmail.com>
2022-10-29 01:15:26 +0900

    페이지네이션완성

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,16 @@
 import { useState } from "react";
 const Pagination = (props) => {
-  const [currPage, setCurrPage] = useState(props.page)
   const numPages = Math.ceil(props.total / props.limit);
   const viewPages = (numPages > 10? 10 : numPages);
-  const pageStart = Math.floor(currPage/ 10);
+  const [pageStart, setPageStart] = useState(0);
   return (
     <>
-      <button onClick={() => {props.setPage(props.page - 1); setCurrPage(props.page);}} disabled={props.page === 1}>
+      <button 
+        onClick={() => {
+          props.setPage(props.page - 1); 
+          setPageStart(Math.floor((props.page - 2) / 10));
+          }} 
+        disabled={props.page === 1}>
         &lt;
       </button>
       {Array(viewPages)
@@ -15,17 +19,22 @@ const Pagination = (props) => {
           if(pageStart * 10 + i + 1 <= numPages) {
             return(
               <button
-            key={pageStart * 10 + i + 1}
-            onClick={() => {
-              props.setPage(pageStart * 10 + i + 1);
-            }}
-            aria-current={props.page === pageStart * 10 + i + 1 ? "page" : null}
-          >
-          {pageStart * 10 + i + 1}
-          </button>
-          )
+                key={pageStart * 10 + i + 1}
+                onClick={() => {
+                  props.setPage(pageStart * 10 + i + 1);
+                }}
+                aria-current={props.page === pageStart * 10 + i + 1 ? "page" : null}
+              >
+                {pageStart * 10 + i + 1}
+              </button>
+            )
         }})}
-      <button onClick={() => {props.setPage(props.page + 1); setCurrPage(props.page);}} disabled={props.page === numPages}>
+      <button 
+        onClick={() =>{
+          props.setPage(props.page + 1); 
+          setPageStart(Math.floor(props.page / 10));}}
+        disabled={props.page === numPages}
+      >
         &gt;
       </button>
     </>
